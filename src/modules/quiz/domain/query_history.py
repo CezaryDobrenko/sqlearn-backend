edbfcde9656867ffadd7714a07b55256ede6a0fd
CezaryDobrenko <cezary.dobrenko@emplocity.pl>
2022-11-03 21:27:17 +0100

    added query_history model/migration

---
+++ src/modules/quiz/domain/query_history.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel
+
+
+class QueryHistory(BaseModel):
+    __tablename__ = "query_history"
+
+    query: str = Column(String)
+    is_valid: bool = Column(Boolean)
+
+    assignment_id: int = Column(
+        Integer(),
+        ForeignKey("assignment.id", ondelete="CASCADE"),
+        index=True,
+        nullable=False,
+    )
+    assignment = relationship(
+        "Assignment", foreign_keys=[assignment_id], back_populates="queries"
+    )
+
+    def __str__(self):
+        return f"QueryHistory({self.title=})"
